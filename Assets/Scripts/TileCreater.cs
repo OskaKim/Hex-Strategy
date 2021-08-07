b8ff1ad6801c536eb37229f6fa78abc3659d7568
Dohyeong Kim <o2kim0225@naver.com>
2021-08-07 22:41:25 +0900

    fix - 인접타일 리스트가 홀수 짝수 반대였기 때문에 고치고, 배열로 구하도록 하기

--- Assets/Scripts/TileCreater.cs
+++ Assets/Scripts/TileCreater.cs
@@ -22,59 +22,62 @@ namespace Tile
             }
         }
 
-        private static Tile GetTile(IndexPair indexPair)
+        public static Tile GetTile(IndexPair indexPair)
         {
             return TileCreater.tiles.FirstOrDefault(x => x.IndexPair == indexPair);
         }
 
-        private static List<Tile> GetNearTiles(Tile tile)
+        public static Tile[] GetNearTiles(Tile tile)
         {
             var x = tile.IndexPair.x;
             var y = tile.IndexPair.y;
-
-            List<Tile> nearTiles = new List<Tile>(getNearTileCount(tile.IndexPair));
             bool isEven = y % 2 == 0;
 
             // NOTE : 육각타일의 특성상 홀수 짝수에서 인덱스가 달라지기 때문에 특수한 방식으로 타일을 습득
             if (isEven)
             {
-                nearTiles.Add(GetTile(new IndexPair(x - 1, y)));
-                nearTiles.Add(GetTile(new IndexPair(x, y - 1)));
-                nearTiles.Add(GetTile(new IndexPair(x, y + 1)));
-                nearTiles.Add(GetTile(new IndexPair(x + 1, y - 1)));
-                nearTiles.Add(GetTile(new IndexPair(x + 1, y)));
-                nearTiles.Add(GetTile(new IndexPair(x + 1, y + 1)));
+                return new Tile[]
+                {
+                    GetTile(new IndexPair(x - 1, y - 1)),
+                    GetTile(new IndexPair(x - 1, y)),
+                    GetTile(new IndexPair(x - 1, y + 1)),
+                    GetTile(new IndexPair(x, y - 1)),
+                    GetTile(new IndexPair(x, y + 1)),
+                    GetTile(new IndexPair(x + 1, y))
+                }
+                .Where(x => x != null).ToArray();
             }
-            else
+
+            return new Tile[]
             {
-                nearTiles.Add(GetTile(new IndexPair(x - 1, y - 1)));
-                nearTiles.Add(GetTile(new IndexPair(x - 1, y)));
-                nearTiles.Add(GetTile(new IndexPair(x - 1, y + 1)));
-                nearTiles.Add(GetTile(new IndexPair(x, y - 1)));
-                nearTiles.Add(GetTile(new IndexPair(x, y + 1)));
-                nearTiles.Add(GetTile(new IndexPair(x + 1, y)));
+                GetTile(new IndexPair(x - 1, y)),
+                GetTile(new IndexPair(x, y - 1)),
+                GetTile(new IndexPair(x, y + 1)),
+                GetTile(new IndexPair(x + 1, y - 1)),
+                GetTile(new IndexPair(x + 1, y)),
+                GetTile(new IndexPair(x + 1, y + 1))
             }
-
-            return nearTiles;
+            .Where(x => x != null).ToArray();
         }
 
-        private static int getNearTileCount(IndexPair index)
-        {
-            var x = index.x;
-            var y = index.y;
+        // TODO : 필요 없을 수도 있는데 혹시 몰라서 주석처리
+        //private static int getNearTileCount(IndexPair index)
+        //{
+        //    var x = index.x;
+        //    var y = index.y;
 
-            // NOTE : 기본적으론 인접타일은 6개지만, 가장자리인 경우엔 따로 정의해야 함.
-            int size = 6;
-            if (x == 0 && y == 0) size = 2;                      // left under
-            else if (x == 0 && y == maxIndexY) size = 3;         // left top
-            else if (x == 0 && y != 0) size = 3;                 // left
-            else if (x != 0 && y == 0) size = 4;                 // under
-            else if (x == maxIndexX && y == maxIndexY) size = 2; // right top
-            else if (x != maxIndexX && y == maxIndexY) size = 4; // top
-            else if (x == maxIndexX) size = 3;                   // right, right under
+        //    // NOTE : 기본적으론 인접타일은 6개지만, 가장자리인 경우엔 따로 정의해야 함.
+        //    int size = 6;
+        //    if (x == 0 && y == 0) size = 2;                      // left under
+        //    else if (x == 0 && y == maxIndexY) size = 3;         // left top
+        //    else if (x == 0 && y != 0) size = 3;                 // left
+        //    else if (x != 0 && y == 0) size = 4;                 // under
+        //    else if (x == maxIndexX && y == maxIndexY) size = 2; // right top
+        //    else if (x != maxIndexX && y == maxIndexY) size = 4; // top
+        //    else if (x == maxIndexX) size = 3;                   // right, right under
 
-            return size;
-        }
+        //    return size;
+        //}
 
     }
     public class TileCreater : MonoBehaviour
