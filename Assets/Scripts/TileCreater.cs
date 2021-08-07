f5dd77e74631ec700c1c51ec5fb67a8fbbe1eb82
Dohyeong Kim <o2kim0225@naver.com>
2021-08-07 22:09:26 +0900

    impl - 타일 헬퍼 클래스와 인접 타일 리스트를 구하는 기능 추가

--- Assets/Scripts/TileCreater.cs
+++ Assets/Scripts/TileCreater.cs
@@ -5,15 +5,89 @@ using System.Linq;
 
 namespace Tile
 {
+    public static class TileHelper
+    {
+        public static int maxIndexX { get; private set; }
+        public static int maxIndexY { get; private set; }
+        public static IndexPair maxIndex
+        {
+            get
+            {
+                return new IndexPair(maxIndexX, maxIndexY);
+            }
+            set
+            {
+                maxIndexX = value.x;
+                maxIndexY = value.y;
+            }
+        }
+
+        private static Tile GetTile(IndexPair indexPair)
+        {
+            return TileCreater.tiles.FirstOrDefault(x => x.IndexPair == indexPair);
+        }
+
+        private static List<Tile> GetNearTiles(Tile tile)
+        {
+            var x = tile.IndexPair.x;
+            var y = tile.IndexPair.y;
+
+            List<Tile> nearTiles = new List<Tile>(getNearTileCount(tile.IndexPair));
+            bool isEven = y % 2 == 0;
+
+            // NOTE : 육각타일의 특성상 홀수 짝수에서 인덱스가 달라지기 때문에 특수한 방식으로 타일을 습득
+            if (isEven)
+            {
+                nearTiles.Add(GetTile(new IndexPair(x - 1, y)));
+                nearTiles.Add(GetTile(new IndexPair(x, y - 1)));
+                nearTiles.Add(GetTile(new IndexPair(x, y + 1)));
+                nearTiles.Add(GetTile(new IndexPair(x + 1, y - 1)));
+                nearTiles.Add(GetTile(new IndexPair(x + 1, y)));
+                nearTiles.Add(GetTile(new IndexPair(x + 1, y + 1)));
+            }
+            else
+            {
+                nearTiles.Add(GetTile(new IndexPair(x - 1, y - 1)));
+                nearTiles.Add(GetTile(new IndexPair(x - 1, y)));
+                nearTiles.Add(GetTile(new IndexPair(x - 1, y + 1)));
+                nearTiles.Add(GetTile(new IndexPair(x, y - 1)));
+                nearTiles.Add(GetTile(new IndexPair(x, y + 1)));
+                nearTiles.Add(GetTile(new IndexPair(x + 1, y)));
+            }
+
+            return nearTiles;
+        }
+
+        private static int getNearTileCount(IndexPair index)
+        {
+            var x = index.x;
+            var y = index.y;
+
+            // NOTE : 기본적으론 인접타일은 6개지만, 가장자리인 경우엔 따로 정의해야 함.
+            int size = 6;
+            if (x == 0 && y == 0) size = 2;                      // left under
+            else if (x == 0 && y == maxIndexY) size = 3;         // left top
+            else if (x == 0 && y != 0) size = 3;                 // left
+            else if (x != 0 && y == 0) size = 4;                 // under
+            else if (x == maxIndexX && y == maxIndexY) size = 2; // right top
+            else if (x != maxIndexX && y == maxIndexY) size = 4; // top
+            else if (x == maxIndexX) size = 3;                   // right, right under
+
+            return size;
+        }
+
+    }
     public class TileCreater : MonoBehaviour
     {
         [SerializeField] private IndexPair tileRange = new IndexPair(1, 1);
         [SerializeField] private Tile tilePrefab;
-        private readonly List<Tile> tiles = new List<Tile>();
+
+        // TODO : 타일 리스트를 어디에 두는게 좋을지
+        public static readonly List<Tile> tiles = new List<Tile>();
 
         private void Create(IndexPair indexRange)
         {
-            tiles.Capacity = indexRange.x * indexRange.y;
+            TileHelper.maxIndex = new IndexPair(indexRange.x, indexRange.y);
 
             for (int y = 0; y < indexRange.y; ++y)
             {
@@ -27,11 +101,16 @@ namespace Tile
             }
         }
 
-        private Tile GetTile(IndexPair indexPair) {
-            return tiles.FirstOrDefault(x => x.IndexPair == indexPair);
+        private void OnEnable()
+        {
+            Create(tileRange);
+        }
+        private void OnDisable()
+        {
+            ClearAll();
         }
 
-        private void ClearAll()
+        private static void ClearAll()
         {
             if (tiles.Count == 0) return;
 
@@ -40,15 +119,6 @@ namespace Tile
                 .ForEach(x => Destroy(x.gameObject));
 
             tiles.Clear();
-        }
-
-        private void OnEnable()
-        {
-            Create(tileRange);
-        }
-        private void OnDisable()
-        {
-            ClearAll();
         }
     }
 }
