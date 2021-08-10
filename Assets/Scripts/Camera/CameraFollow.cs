33f1f8e19f3c9a5c1caecfdf483d05e8613ac2f3
Dohyeong Kim <o2kim0225@naver.com>
2021-08-10 22:11:26 +0900

    impl - 카메라 팔로우와 컨트롤

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace MyCamera
+{
+    [ExecuteInEditMode]
+    public class CameraFollow : MonoBehaviour
+    {
+        [SerializeField] private CameraModel cameraModel;
+
+        private void Start()
+        {
+            Observable.EveryLateUpdate()
+            .Where(_ => cameraModel.GetTarget())
+            .Subscribe(_ =>
+            {
+                var targetPos = cameraModel.GetTarget().position;
+                var desiredPosition = new Vector3(targetPos.x, transform.position.y, targetPos.y) + cameraModel.GetFollowOffset();
+                var smoothPosition = Vector3.Lerp(transform.position, desiredPosition, cameraModel.GetSmoothSpeed() * Time.deltaTime);
+                transform.position = smoothPosition;
+            });
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/CameraModel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyCamera
+{
+    public class CameraModel : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private float smoothSpeed = 20.0f;
+        [SerializeField] private Vector3 followOffset = new Vector3(0, 0, 0);
+
+        public Transform GetTarget() { return target; }
+        public float GetSmoothSpeed() { return smoothSpeed; }
+        public Vector3 GetFollowOffset() { return followOffset; }
+    }
+}
