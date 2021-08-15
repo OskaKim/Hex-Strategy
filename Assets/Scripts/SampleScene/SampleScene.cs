856d91356a94924e4ac1c00489b4b96a347b6c4a
Dohyeong Kim <o2kim0225@naver.com>
2021-08-15 19:20:58 +0900

    fix - 타일 리소스를 경로로부터 생성하도록 수정

---
+++ Assets/Scripts/SampleScene/SampleScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 리소스 반영 확인용
+public class SampleScene : MonoBehaviour
+{
+    [SerializeField] Tile.Tile tilePrefab;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
