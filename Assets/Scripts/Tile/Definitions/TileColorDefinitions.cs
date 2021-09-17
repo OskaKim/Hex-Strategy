c50434402c2252bb61ee46790bf8d9c192607729
Dohyeong Kim <o2kim0225@naver.com>
2021-09-17 21:43:04 +0900

    impl - 기후 컬러 추가

--- Assets/Scripts/Tile/Definitions/TileColorDefinitions.cs
+++ Assets/Scripts/Tile/Definitions/TileColorDefinitions.cs
@@ -25,5 +25,15 @@ namespace Tile {
             new Color(0,1,1,1),
             new Color(1,1,1,1),
         };
+        public static Color GetClimateColor(int climateType) {
+            if (climateType == -1) return new Color(0, 0, 0, 1);
+            return climateColor[climateType];
+        }
+        private static Color[] climateColor = {
+            new Color(0.9f, 0.99f, 0.83f),  // NOTE : 한대기후
+            new Color(0.16f, 0.81f, 0.83f), // NOTE : 냉대기후
+            new Color(0.98f, 0.78f, 0.38f), // NOTE : 온대기후
+            new Color(0.87f, 0.2f, 0.06f)   // NOTE : 열대기후
+        };
     }
 }
