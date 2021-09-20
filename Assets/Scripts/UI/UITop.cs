9d496564cb0f7bd74c50fa74ad183e9808e51e1f
Dohyeong Kim <o2kim0225@naver.com>
2021-09-20 11:10:28 +0900

    rename - UITopLens -> UITop

--- Assets/Scripts/UI/UITop.cs
+++ Assets/Scripts/UI/UITop.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UITopLens : MonoBehaviour
+public class UITop : MonoBehaviour
 {
     enum LensType {
         None,
