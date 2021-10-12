754c820d953c96597be07dcfda41b4aad5cf38ca
Dohyeong Kim <o2kim0225@naver.com>
2021-10-12 21:20:15 +0900

    impl - next button click event

---
+++ Assets/Scripts/UI/UIButtomRight.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtomRight : MonoBehaviour
+{
+    public void OnClickNextTurn() {
+        Debug.Log("OnCLickedNextTurn");
+    }
+}
