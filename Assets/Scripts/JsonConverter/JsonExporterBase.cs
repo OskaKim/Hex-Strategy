b716b4509512db447880c9c0cf80a26c63765c8d
Dohyeong Kim <o2kim0225@naver.com>
2021-11-07 16:11:46 +0900

    fix - 디렉토리가 없으면 생성. 예외처리도 추가

--- Assets/Scripts/JsonConverter/JsonExporterBase.cs
+++ Assets/Scripts/JsonConverter/JsonExporterBase.cs
@@ -6,8 +6,20 @@ public class JsonConverterBase {
 
     // NOTE : 해당 데이터를 json으로 저장
     protected static void ExportToJson(object data, string path, string fileName) {
-        var json = JsonUtility.ToJson(data, true);
-        var saveFile = $"{DataPath}/{path}/{fileName}";
-        File.WriteAllText(saveFile, json);
+        var filePath = $"{DataPath}/{path}";
+        try {
+            if (!Directory.Exists(filePath)) {
+                Directory.CreateDirectory(filePath);
+                Debug.Log($"Directory {filePath} has created!");
+            }
+
+            var json = JsonUtility.ToJson(data, true);
+            var saveFile = $"{DataPath}/{path}/{fileName}";
+            File.WriteAllText(saveFile, json);
+            Debug.Log($"Json {saveFile} has written!");
+        }
+        catch (IOException ex) {
+            Debug.Log(ex.Message);
+        }
     }
 }
