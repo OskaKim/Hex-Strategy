513d4d206bb1c4a7476dc636cce0cff9be8f2c04
Dohyeong Kim <o2kim0225@naver.com>
2021-11-07 15:49:55 +0900

    impl - 생성된 타일의 터레인 정보를 json에 저장

--- Assets/Scripts/Data/TileData.cs
+++ Assets/Scripts/Data/TileData.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using Tile;
+using System.IO;
+
+[System.Serializable]
+public class TileJsonData {
+    [System.Serializable]
+    public class TileJsonDataElement {
+        public int terrainType;
+        public int x;
+        public int y;
+    }
+
+    public List<TileJsonDataElement> tileDataList;
+}
 
 public class TileData : MonoBehaviour {
     public static void SetTileTerrainData() {
+        var data = new TileJsonData();
         var allTiles = TileModel.tiles;
-        var terrainDic = allTiles.Select(x => new { x.TerrainType, x.IndexPair });
-
-        terrainDic.All(x => {
-            Debug.Log($"{x.TerrainType} : {x.IndexPair.X}, {x.IndexPair.Y}");
-            return true;
-        });
+        data.tileDataList = allTiles.Select(x => new TileJsonData.TileJsonDataElement() { terrainType = (int)x.TerrainType, x = x.IndexPair.X, y = x.IndexPair.Y }).ToList();
+        var json = JsonUtility.ToJson(data, true);
+        var saveFile = Application.dataPath + "/terrainData.json";
+        File.WriteAllText(saveFile, json);
     }
 }
